ad29b8b453d66ddf81e1962956d52a8da10a634d
AndyGerrity <andrew.gerrity@msci.com>
2016-11-06 22:16:35 +0000

    Update Plot4.R

--- Plot4.R
+++ Plot4.R
@@ -1,9 +1,12 @@
+#load data
 powercon <- read.table("household_power_consumption.txt",skip=1,sep=";")
 
 names(powercon) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
+#subset data
 powercon2007 <- subset(powercon,powercon$Date=="1/2/2007" | powercon$Date =="2/2/2007")
 
+#convert data to correct format
 datetime <- strptime(paste(powercon2007$Date, powercon2007$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 globalactivepower <- as.numeric(powercon2007$Global_active_power)
@@ -17,6 +20,8 @@ submetering3 <- as.numeric(powercon2007$Sub_metering_3)
 globalreactivePower <- as.numeric(subSetData$Global_reactive_power)
 
 voltage <- as.numeric(powercon2007$Voltage)
+
+#call plot functions
 
 png("plot4.png", width=480, height=480)
 par(mfrow = c(2, 2)) 
