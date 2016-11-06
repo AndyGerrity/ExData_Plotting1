32f5819224193f202d23ffd9fb9c71617216d35b
AndyGerrity <andrew.gerrity@msci.com>
2016-11-06 22:15:53 +0000

    Update Plot3.R

--- Plot3.R
+++ Plot3.R
@@ -1,10 +1,12 @@
+#load data
 powercon <- read.table("household_power_consumption.txt",skip=1,sep=";")
 
 names(powercon) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
+#subset data
 powercon2007 <- subset(powercon,powercon$Date=="1/2/2007" | powercon$Date =="2/2/2007")
 
-
+#convert subset to correct format
 datetime <- strptime(paste(powercon2007$Date, powercon2007$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 globalactivepower <- as.numeric(powercon2007$Global_active_power)
@@ -16,7 +18,7 @@ submetering2 <- as.numeric(powercon2007$Sub_metering_2)
 submetering3 <- as.numeric(powercon2007$Sub_metering_3)
 
 
-# calling the basic plot functions
+# call the plot functions
 png("plot3.png", width=480, height=480)
 
 plot(datetime, submetering1, type="l", ylab="Energy Submetering", xlab="")
