5305912fca12e785409d919b3387cec24b14e89f
AndyGerrity <andrew.gerrity@msci.com>
2016-11-06 22:15:07 +0000

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,12 +1,15 @@
+#load data
 powercon <- read.table("household_power_consumption.txt",skip=1,sep=";")
 
+#subset data
 powercon2007 <- powercon[powercon$Date %in% c("1/2/2007","2/2/2007") ,]
 
-#str(subSetData)
+#convert subset to correct format
 datetime <- strptime(paste(powercon2007$Date, powercon2007$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
 
 globalActivePower <- as.numeric(powercon2007$Global_active_power)
 
+#call plot funciton
 png("plot2.png", width=480, height=480)
 
 plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
