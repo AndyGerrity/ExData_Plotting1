6f9917362f606e738efe3c6092454c8aedf76dc1
AndyGerrity <andrew.gerrity@msci.com>
2016-11-06 22:14:08 +0000

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -1,9 +1,12 @@
+#load data
 powercon <- read.table("household_power_consumption.txt",skip=1,sep=";")
 
 names(powercon) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
 
+#subset data
 powercon2007 <- subset(powercon,powercon$Date=="1/2/2007" | powercon$Date =="2/2/2007")
 
+#call plot function
 png("plot1.png", width=480, height=480)
 
 hist(as.numeric(as.character(powercon2007$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
